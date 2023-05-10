e57a6294b454b7d931a8ecd7107e920ffe60ee54
Gopinath <133114658+Gopinath-King@users.noreply.github.com>
2023-05-10 19:57:38 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,8 @@
+// Java Simple Program
+import java.lang.*;
+public class Simple {
+        public static void main(String[] args)
+        {
+            System.out.println("Hello World...!");
+        }
+}
